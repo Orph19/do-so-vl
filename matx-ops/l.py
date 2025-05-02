83df06d387fb93f6397ebbd4ceb6bea593273b7c
Orph <behrmantillapolo@gmail.com>
2025-05-02 14:50:54 -0500

    working with numpy

--- matx-ops/l.py
+++ matx-ops/l.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python3
 import numpy as np
+np.random.seed(0)
+# rd = np.random.randn(50)
+# ud = np.random.rand(50)
 
-od = np.random.randint(10,31, size=5)
-twd = np.random.randint(1,34, size = (4,4))
-thd = np.random.randint(6,67,size=(2,5,6))
-print("{}".format(twd))
-# print("{}\n{}\n{}".format(od.ndim,od.shape,od.size))
+# # print(np.mean(rd),np.min(rd),np.max(rd),np.std(rd))
 
-print(twd[::-1,::-1])
+# # print(np.mean(ud),np.std(ud))
+# print(sum(np.where(rd>0.5,True,False)))
+
+# ma = np.zeros((5,5),dtype=float)
+# ma[1:4,1:4] = 1
+# mb = np.ones((5,5),dtype=float)*5
+# wt = ma*1.5
+# print(wt.T[:,1:2])
+
+total_time = 10.0
+num_steps = 50
+t = np.linspace(0,total_time,num_steps)
+
+
+initial_position = 0.0
+constant_velocity = 2.5
+
+positions = np.zeros(num_steps)
+positions = initial_position + constant_velocity*t
+print(positions)
--- matx-ops/npmatrix.py
+++ matx-ops/npmatrix.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 import numpy as np
 
-class VectorND:
+class Vector:
     def __init__(self,vector):
         self.vector = np.array(vector).reshape(vector.size)
-
 
     def __add__(self,ot):
         return self.vector + ot.vector
@@ -14,17 +13,10 @@ class VectorND:
         return np.dot(self.vector,ot.vector)
     def scale(self,scale):
         return self.vector *scale
-    def magntide():
-        pass
-    
-a = VectorND(np.arange(0,10).reshape(5,2))
-b = VectorND(np.arange(5,15).reshape(5,2))
 
 
-print("{} \n{} \n\n{}".format(a.vector,b.vector,a+b),
-      "the dot product of {} and{} is {}".format(a.vector,b.vector,a.dot(b)),
-      sep="\n")
-# print(vector(a))
+
+print(a)
 
 
 
